fb9ad91acaeb0879ed7f10b3d0f635e5ac64d50e
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-07-13 02:59:34 +0530

    Completed the Frontend Part

---
+++ market_place-maker/Connections/Functionality/realPFS.js
@@ -0,0 +1,30 @@
+async function IpfsToArray(urls) {
+  // Define the URLs to fetch data from
+  console.log(urls);
+
+  // Use Promise.all to fetch all URLs concurrently
+  const fetchPromises = urls.map((url) =>
+    fetch(url).then((response) => response.json())
+  );
+
+  try {
+    // Wait for all fetch operations to complete
+    const results = await Promise.all(fetchPromises);
+
+    // Combine the results into a single JSON object
+    // const combinedData = results.reduce((acc, data, index) => {
+    //   acc[`data${index + 1}`] = data;
+    //   return acc;
+    // }, {});
+
+    // Log the combined JSON object
+    console.log(results);
+
+    return results;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+// Call the function to fetch data
+export default IpfsToArray;
